7972efb92fe25d7039b87e2ae028717d71171b69
almaninyo <almaninyo@yandex.ru>
2020-12-14 20:25:57 +0100

    Map - core and data_domain folders

---
+++ lib/map/data_domain/models/building.dart
@@ -0,0 +1,34 @@
+import 'package:meta/meta.dart';
+
+//Data Layer - Entity
+class Building {
+  final String campus;
+  final String name;
+  final int floors;
+
+  Building({
+    @required this.floors,
+    @required this.campus,
+    @required this.name
+  });
+}
+
+//Domain Layer - Model
+class BuildingModel extends Building {
+  BuildingModel({
+    @required String name,
+    @required String campus,
+    @required int floors,
+  }):super(name: name,campus: campus,floors: floors);
+
+  factory BuildingModel.fromJson(Map<String, dynamic> jsonBuilding) {
+    return BuildingModel (floors: jsonBuilding['floors'],campus: jsonBuilding['campus'],name: jsonBuilding['name']);
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "campus": campus,
+      "floors": floors,
+    };
+  }
+}
---
+++ lib/map/data_domain/repositories/room_repository.dart
@@ -0,0 +1,112 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:schedule_dva232/map/data_domain/models/room.dart';
+import 'package:meta/meta.dart';
+import 'package:schedule_dva232/map/core/error/exceptions.dart';
+import 'package:schedule_dva232/map/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:schedule_dva232/map/data_domain/models/building.dart';
+import 'package:latlong/latlong.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+//Domain Layer
+abstract class RoomRepository {
+  Future <Either<Failure,Room>> getRoom(String name);
+  Future <Either<Failure,Room>> getLastRoom();
+}
+
+//Data Layer
+abstract class RoomAssetsDataSource {
+  Future <RoomModel> getRoom(String name);
+}
+
+abstract class RoomCacheDataSource {
+  Future <RoomModel> getLastRoom();
+  Future<void> cacheRoom(RoomModel roomToCache);
+}
+
+class RoomAssetsDataSourceImpl implements RoomAssetsDataSource {
+
+  @override
+  Future<RoomModel> getRoom (String name)  async {
+    final String buildings = await rootBundle.loadString("assets/buildings_rooms.json");
+    Map<String, dynamic> jsonBuildings = json.decode(buildings);
+    for (Map<String, dynamic> building in jsonBuildings['buildings']) {
+      for (Map<String, dynamic> room in building['rooms']) {
+        if (room['name'] == name) {
+          //if room is found
+          print('the room is found');
+          print (room['position']['lat']);
+          print(room['position']['lng']);
+          return Future.value(RoomModel(
+              building: Building (floors: building['floors'], name:building['name'], campus: building['campus']),
+              name: room['name'],
+              floor: room['floor'],
+              position: LatLng(
+                  room['position']['lat'], room['position']['lng'])));
+        }
+      }
+    }
+    //room not found
+    throw AssetsException();
+  }
+}
+
+class RoomCacheDataSourceImpl implements RoomCacheDataSource {
+  final SharedPreferences sharedPreferences;
+  RoomCacheDataSourceImpl({@required this.sharedPreferences});
+  @override
+  Future<RoomModel> getLastRoom() {
+    final jsonStringBuilding = sharedPreferences.getString('CACHED_ROOM');
+    if(jsonStringBuilding != null) {
+      return Future.value(RoomModel.fromJson(json.decode(jsonStringBuilding)));
+    }
+    else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheRoom(RoomModel roomToCache) {
+    return sharedPreferences.setString('CACHED_ROOM', json.encode(roomToCache.toJson()));
+  }
+}
+
+
+
+class RoomRepositoryImpl implements RoomRepository {
+  final RoomCacheDataSource cacheDataSource;
+  final RoomAssetsDataSource assetsDataSource;
+
+  RoomRepositoryImpl({
+    @required this.cacheDataSource,
+    @required this.assetsDataSource
+  });
+
+  @override
+  Future<Either<Failure, Room>> getRoom(String name) async {
+    //Get the room data from assets
+    try {
+      final roomToCache = await assetsDataSource.getRoom(name);
+      // Remember the room
+      cacheDataSource.cacheRoom(roomToCache);
+      return Right(roomToCache);
+    }
+    //If fails to get room data
+    on AssetsException {
+      return Left(AssetsFailure());
+    }
+  }
+  @override
+  Future<Either<Failure, Room>> getLastRoom() async {
+    try {
+      final cacheRoom = await cacheDataSource.getLastRoom();
+      return Right(cacheRoom);
+    }
+    on CacheException {
+      return Left(CacheFailure());
+    }
+  }
+}
