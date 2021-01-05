ef25c78d56dac78ff9dde88564a36022e5eb1ee6
Emelie Wallin <emeliewallin@hotmail.com>
2021-01-05 14:33:11 +0100

    exam notifications, schedule updateChecks

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:schedule_dva232/routing.dart';
+import 'package:schedule_dva232/schedule/subfiles/scheduleUpdater.dart';
 import 'package:stacked_themes/stacked_themes.dart';
 import 'package:schedule_dva232/theme/themes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:schedule_dva232/injection_container.dart' as ic;
 import 'package:workmanager/workmanager.dart';
+import 'package:cron/cron.dart';
+import 'package:schedule_dva232/globalNotification.dart' as global;
 
 import 'notification/notificationHandler.dart';
+import 'notification/updateCheck.dart';
 
 var theme;
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Workmanager.initialize(callbackDispatcher, isInDebugMode: true);
-  Workmanager.registerPeriodicTask('2', 'simpelPeriodTask', frequency: Duration(minutes: 15));
+
+
+  var cron = new Cron();
+  cron.schedule(Schedule.parse('* * * * *'), () async { // '0 * * * *' schedule update checks once every hour
+    parseSchedule();
+    print(global.newItem);
+    Future.delayed(Duration(seconds: 1), () async{
+      if(global.newItem){
+        global.notificationList.forEach((element) {
+          print('testar loopa listan');
+          Future.delayed(Duration(seconds: 2), (){
+            Workmanager.initialize(callbackDispatcher, isInDebugMode: false); //debugMode is only to help with debugging
+            Workmanager.registerOneOffTask('1', 'simpelTask', inputData: {'string': '${element.content}'
+            });
+         });
+        });
+      }
+    });
+
+  });
+
+
   await ic.init();
   await ThemeManager.initialise();
   SharedPreferences localStorage = await SharedPreferences.getInstance();
@@ -30,7 +55,7 @@ Future main() async {
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return  ThemeBuilder(
+    return ThemeBuilder(
       defaultThemeMode: theme,
       darkTheme: AppTheme.darkTheme,
       lightTheme: AppTheme.lightTheme,
@@ -39,17 +64,7 @@ class App extends StatelessWidget{
         darkTheme: darkTheme,
         themeMode: themeMode,
         initialRoute: '/',
-       onGenerateRoute: Roots.generateRoute,
-       /* routes: {
-            '/': (context) => LoginMain(),
-            '/schedule': (context) => Schedule(),
-            '/scheduleSettings': (context) => ScheduleSettings(),
-            '/addCourse': (context) => AddCourse(),
-            '/thisweek': (context) => Thisweek(),
-            '/map': (context) => IntroMapPage(),
-            '/browsing': (context) => BrowsingPage(arguments)
-            '/settings': (context) => Settings(),
-          },*/
+        onGenerateRoute: Roots.generateRoute,
       ),
     );
   }
