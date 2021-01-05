d102e203f65d43f504edb0364497895bb3189b67
almaninyo <almaninyo@yandex.ru>
2021-01-05 15:22:16 +0100

    Settings-Renderoverflow

--- lib/generalPages/settings.dart
+++ lib/generalPages/settings.dart
@@ -22,104 +22,107 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40.0,
-            ),
-            Image.asset('assets/logo/LogoLightTheme.png', width: 80.0, height: 80.0),
-            SizedBox(
-              height: 20.0,
-            ),
-            FutureBuilder(
-              //Will change the button label depending on if the user is logged in or not
-              future: _loggedIn,
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                    return Text('Something went wrong');
-                    break;
-                  case ConnectionState.active:
-                    return SizedBox(
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width,
-                        child: Center(child: CircularProgressIndicator()));
-                    break;
-                  case ConnectionState.done:
-                    if (snapshot.data == true) {
-                      return loggedIn();
-                    } else {
-                      return notLoggedIn();
-                    }
-                    break;
-                  default:
-                    return Text("Unexpected error");
-                }
-              },
-            ),
-            ElevatedButton(
-              child: Text("Manage courses"),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/scheduleSettings');
-              },
-            ),
-            ListTile(
-              leading: Text("Dark theme"),
-              trailing: Switch(
-                value: _darkModeSwitch,
-                onChanged: (value) async {
-                  SharedPreferences localStorage =
-                  await SharedPreferences.getInstance();
-                  if (localStorage.getBool('theme')) {
-                    //if lightmode when change set to false to get darkmode
-                    localStorage.setBool('theme', false);
-                  } else {
-                    //if darkmode when change set to true to get lightmode
-                    localStorage.setBool('theme', true);
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 40.0,
+              ),
+              Image.asset('assets/logo/LogoLightTheme.png', width: 80.0, height: 80.0),
+              SizedBox(
+                height: 20.0,
+              ),
+              FutureBuilder(
+                //Will change the button label depending on if the user is logged in or not
+                future: _loggedIn,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                      return Text('Something went wrong');
+                      break;
+                    case ConnectionState.active:
+                      return SizedBox(
+                          width: MediaQuery
+                              .of(context)
+                              .size
+                              .width,
+                          child: Center(child: CircularProgressIndicator()));
+                      break;
+                    case ConnectionState.done:
+                      if (snapshot.data == true) {
+                        return loggedIn();
+                      } else {
+                        return notLoggedIn();
+                      }
+                      break;
+                    default:
+                      return Text("Unexpected error");
                   }
-                  //Change the theme
-                  getThemeManager(context).toggleDarkLightTheme();
-
-                  setState(() {
-                    _darkModeSwitch = value;
-                  });
                 },
               ),
-            ),
-
-
-            /*******************************************
-             * Only for testing
-             *******************************************/
-            SizedBox(
-              height: 150.0,
-            ),
-            Text(
-              "Only for testing. Remove",
-              style: TextStyle(color: Colors.deepOrange),
-            ),
-            FlatButton.icon(
-              onPressed: () async {
-                SharedPreferences localStorage =
+              ElevatedButton(
+                child: Text("Manage courses"),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/scheduleSettings');
+                },
+              ),
+              ListTile(
+                leading: Text("Dark theme"),
+                trailing: Switch(
+                  value: _darkModeSwitch,
+                  onChanged: (value) async {
+                    SharedPreferences localStorage =
                     await SharedPreferences.getInstance();
-                await localStorage.remove('rawSchedule');
-                await localStorage.remove('token');
-                await localStorage.remove('course_list');
-                await localStorage.remove('course_color');
-                Navigator.pushReplacementNamed(context, '/');
-              },
-              icon: Icon(Icons.remove_circle),
-              label: Text('Remove User Localstorage (temp)'),
-            ),
-            ElevatedButton(
-              child: Text("Return to main, without signing out"),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/');
-              },
-            ),
-          ],
+                    if (localStorage.getBool('theme')) {
+                      //if lightmode when change set to false to get darkmode
+                      localStorage.setBool('theme', false);
+                    } else {
+                      //if darkmode when change set to true to get lightmode
+                      localStorage.setBool('theme', true);
+                    }
+                    //Change the theme
+                    getThemeManager(context).toggleDarkLightTheme();
+
+                    setState(() {
+                      _darkModeSwitch = value;
+                    });
+                  },
+                ),
+              ),
+
+
+              /*******************************************
+               * Only for testing
+               *******************************************/
+              SizedBox(
+                height: 150.0,
+              ),
+              Text(
+                "Only for testing. Remove",
+                style: TextStyle(color: Colors.deepOrange),
+              ),
+              FlatButton.icon(
+                onPressed: () async {
+                  SharedPreferences localStorage =
+                      await SharedPreferences.getInstance();
+                  await localStorage.remove('rawSchedule');
+                  await localStorage.remove('token');
+                  await localStorage.remove('course_list');
+                  await localStorage.remove('course_color');
+                  Navigator.pushReplacementNamed(context, '/');
+                },
+                icon: Icon(Icons.remove_circle),
+                label: Text('Remove User Localstorage (temp)'),
+              ),
+              ElevatedButton(
+                child: Text("Return to main, without signing out"),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/');
+                },
+              ),
+            ],
+          ),
         ),
       );
   }
